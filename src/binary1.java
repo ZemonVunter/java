07f04ec53c9a84d4140813bfcb4e352e7ac6ae7a
ZemonVunter <1752767673@qq.com>
2020-04-12 20:57:41 +0800

    统计数字二进制中1的个数

---
+++ src/binary1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class binary1 {
+
+        public static void main(String[] args) {
+            //求一个整数，在内存当中存储时，二进制1的个数
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("请输入一个整数");
+            int num = scanner.nextInt();
+            int count = 0;
+            for(int i = 0;i < 32;i++) {
+                if(((num >> i) & 1) == 1) {
+                    count++;
+                }
+            }
+            System.out.println(count);
+        }
+    }
+
